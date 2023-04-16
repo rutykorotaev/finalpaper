43ce8158498b9fb0e1335538a7e16459734dd492
rutykorotaev@mail.utoronto.ca <ruty.korotaiev@mail.utoronto.ca>
2023-04-16 15:06:01 -0400

    changes

--- finalpaper/scripts/04-test_data.R
+++ finalpaper/scripts/04-test_data.R
@@ -9,6 +9,21 @@
 
 #### Workspace setup ####
 library(tidyverse)
-# [...UPDATE THIS...]
 
 #### Test data ####
+
+#Test 1: Use unique function to ensure there are no duplicates in dataset
+russian_performers |> unique()
+ukrainian_performers |> unique()
+
+#Test 2: Use validator and is.numeric function to ensure all values are regarded as numeric
+
+library(validate)
+rules <- validator(
+  is.numeric(artist_name),
+  is.numeric(album_release_year),
+  is.numeric(danceability),
+  is.numeric(energy),
+  is.numeric(key),
+  is.numeric(mode),
+)
